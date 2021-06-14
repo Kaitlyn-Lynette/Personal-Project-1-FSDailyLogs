7b103df222bce5065b2113cc94a9550211152777
Kaitlyn Rodriguez <kaitlyn.l.rodriguez@gmail.com>
2021-06-14 17:50:08 -0400

    Create a way to read the existing file and then push that datdata to another  file

--- index.js
+++ index.js
@@ -1,111 +1,63 @@
-const inquirer = require ("inquirer");
-const fs = require("fs");
-const util = require("util");
-// const { inherits } = require("util");
-
-// const writeFileAsync = util.promisify(fs.writeFile);
-// const readFileAsync = util.promisify(fs.readFile);
-let data = fs.readFileSync("logs.json");
-let logs = JSON.parse(data);
-// const appendFileAsync = util.promisify(fs.appendFile);
-// const logsArr = [];
-
-inquirer.registerPrompt("date", require("inquirer-date-prompt"));
-
-//This function with prompt me with what I do each day 
-// function promptMe() {
-//     // const response = await inquirer.prompt([
-//         return 
-inquirer.prompt ([
-        {
-            type: "number",
-            name: "log number"
-        },
-        {
-            type: "date",
-            name: "date"
-        },
-        {
-            type: "input",
-            message: "What coding logs do you have for today?",
-            name: "input"
-        },
-    ]).then(function(response) {
-        //Define data
-        let newLogs = response
-        console.log(response)
-        //Adding new data to our object
-        logs.push(newLogs)
-        //Write to the JSON file 
-        let addLogs = JSON.stringify(logs);
-        fs.appendFile("logs.json", addLogs,(err) => {
-            if(err) throw err;
-            console.log ("New logs added")
-        })
-    })
-
-// }
-    // console.log(response);
-    // fs.writeFile("logs.json", JSON.stringify(response, null, '\t'), function (err) {
-    //     if (err) {
-    //         return console.log(err);
-    //     }
-    //     console.log("Yay. It Worked!");
-    // });
-
-// function generateHTML(logs) {
-//     return `
-// <!DOCTYPE html>
-// <html lang="en">
-// <head>
-//   <meta charset="UTF-8">
-//   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-//   <link rel="stylesheet" type="text/css" href="style.css">
-//   <link rel="preconnect" href="https://fonts.gstatic.com">
-//   <link href="https://fonts.googleapis.com/css2?family=Courier+Prime&display=swap" rel="stylesheet">
-//   <title>Coding Logs at Sea</title>
-// </head>
-// <body>
-//     <div class="container">
-//     <div class="header-section">
-//         <div class="name-section">
-//             <h1 class="name">Kaitlyn Lynette</h1>
-//             <h2>My Coding Logs at Sea</h2>
-//         </div>
-//     </div>
-//     <div class="logs-section">
-//         <div class="date">
-//             <p>The day at sea is ${logs.date}</p>
-//         </div>
-//         <div class="logs">
-//             <p>${logs.input}</p>
-//         </div>
-//     </div>
-//     </div>
-// </body>
-// </html>`;
-// }
+// const inquirer = require ("inquirer");
+// const fs = require("fs");
+// const util = require("util");
+// inquirer.registerPrompt("date", require("inquirer-date-prompt"));
 
 
-// async function init() {
-//     console.log("this is working, stay calm")
+// // const readFileAsync = util.promisify(fs.readFile);
+// // const appendFileAsync = util.promisify(fs.appendFile);
+// // const writeFileAsync = util.promisify(fs.appendFile);
 
-//     try{
-//         const logs = await promptMe();
-//         // const html = generateHTML(logs);
-//         // await writeFileAsync("index.html", html);
-//         // await appendFileAsync("logs.json", JSON.stringify(logs, null, '\t'));
-//         await readFileAsync("./logs.json",'utf-8',function (err,logs) ) {
-//             if(err) throw err 
-
-//             let arrayOfLogs = JSON.parse(data)
-//             arrayOfLogs.logs.push({logs})
+// //This function with prompt me with what I do each day 
+// inquirer.prompt ([
+//         {
+//             type: "number",
+//             name: "log number"
+//         },
+//         {
+//             type: "date",
+//             name: "date"
+//         },
+//         {
+//             type: "input",
+//             message: "What coding logs do you have for today?",
+//             name: "input"
 //         }
-        
-//         console.log("Successfuly wrote to index.html");
-//     } catch (err) {
-//         console.log(err);
-//     }
-// }
+//     ]).then(function(response) {
+//     // console.log(response);
+//     const logsArr = new Array;
+//     logsArr.push(response)
+//     console.log(logsArr)
+//     // console.log(logs);
+//     fs.writeFile("logs.json", JSON.stringify(logsArr, null, '\t'), function(err){
+//         if (err) {
+//         return console.log(err)
+//         }
+//         console.log("Yay. It Worked!");
+//     }); 
+// })
 
-// init();
+
+const fs = require ("fs");
+const util = require ("util");
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+async function createLogs() {
+    try {
+        const logs = []
+        const fileStr = await readFileAsync("logs.json", "utf8");
+        logs.push(JSON.parse(fileStr))
+
+        await writeFileAsync (
+        "combinedLogs.json", 
+        JSON.stringify(logs, null,2), 
+        "utf8"
+        )
+    } catch  (err) {
+        console.log(err)
+    }
+}
+
+createLogs();
