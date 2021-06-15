b9204f841114d465257105c710623a0ee01263e8
Kaitlyn Rodriguez <kaitlyn.l.rodriguez@gmail.com>
2021-06-15 07:50:41 -0400

    We're reading the old logs and rewriting with new logs. Formatting is almost there.

--- index.js
+++ index.js
@@ -39,16 +39,41 @@
 
 
 const fs = require ("fs");
+const inquirer = require("inquirer");
 const util = require ("util");
+inquirer.registerPrompt("date", require("inquirer-date-prompt"));
 
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
+
+function promptUser()  {
+        return inquirer.prompt([
+        {
+            type: "number",
+            name: "log number"
+        },
+        {
+            type: "date",
+            name: "date"
+        },
+        {
+            type: "input",
+            message: "What coding logs do you have for today?",
+            name: "input"
+        }
+    ]);
+}
+
+
 async function createLogs() {
     try {
         const logs = []
-        const fileStr = await readFileAsync("logs.json", "utf8");
+        const fileStr = await readFileAsync("combinedLogs.json", "utf8");
         logs.push(JSON.parse(fileStr))
+        const answers = await promptUser();
+        logs.push(answers);
+        console.log(logs)
 
         await writeFileAsync (
         "combinedLogs.json", 
